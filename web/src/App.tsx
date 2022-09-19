8e21ddba80493d4124b730f017f21014088453e4
Maicon Henrique <maiconhenriquefa@gmail.com>
2022-09-19 20:51:51 -0300

    add game images mocked

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,6 +6,61 @@ function App() {
   return (
     <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
       <img src={logoImg} alt="" />
+
+      <h1 className="text-6xl text-white font-bold mt-16">
+        Seu <span className="bg-nlw-gradient bg-clip-text text-transparent">duo</span> está aqui.
+      </h1>
+
+      <div className="grid grid-cols-6 gap-6 mt-12">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image-1.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0 left-0">
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image-2.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0 left-0">
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image-3.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0 left-0">
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image-4.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0 left-0">
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image-5.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0 left-0">
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/image-6.png" alt="" />
+
+          <div className="w-full pt-16 pb-4 bg-game-gradient absolute bottom-0 right-0 left-0">
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className="text-zinc-300 text-sm block">4 anúncios</span>
+          </div>
+        </a>
+      </div>
     </div>
   )
 }
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -8,6 +8,8 @@ module.exports = {
     extend: {
       backgroundImage: {
         galaxy: "url('/background.png')",
+        'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 10.08%, #43E7AD 83.94%, #E1D55D 7.57%)',
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
