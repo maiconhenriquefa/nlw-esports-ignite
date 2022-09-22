2310200209442216071c016f2ffbdd3f133a7ca5
Maicon Henrique <maiconhenriquefa@gmail.com>
2022-09-21 21:51:10 -0300

    started create routes

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,18 @@ import express from 'express';
 
 const app = express()
 
+app.get('/games', (request, response) => {
+  return response.json([]);
+});
+
+app.post('/ads', (request, response) => {
+  return response.status(201).json([]);
+});
+
 app.get('/ads', (request, response) => {
   return response.json([
     {id: 1, name: 'um'},
-    {id: 1, name: 'um'},
+    {id: 1, name: 'dois'},
     {id: 1, name: 'um'},
     {id: 1, name: 'um'},
   ])
